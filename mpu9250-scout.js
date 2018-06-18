2090dd76a973945b0d64c2a1312285776d1fda91
Agilatech <scott@agilatech.com>
2018-06-18 09:50:44 -0600

    Initial commit

---
+++ mpu9250-scout.js
@@ -0,0 +1,50 @@
+const config = require('./config');
+const Scout = require(process.lynxari.scout);
+const Mpu = require('./mpu9250');
+
+module.exports = class MpuScout extends Scout {
+
+  constructor(opts) {
+
+    super();
+
+    if (typeof opts !== 'undefined') {
+      // copy all config options defined in the server
+      for (const key in opts) {
+        if (typeof opts[key] !== 'undefined') {
+          config[key] = opts[key];
+        }
+      }
+    }
+
+    if (config.name === undefined) { config.name = "MPU9250" }
+    this.name = config.name;
+
+    this.mpu9250 = new Mpu(config);
+
+  }
+
+  init(next) {
+    const query = this.server.where({name: this.name});
+  
+    const self = this;
+
+    this.server.find(query, function(err, results) {
+      if (!err) {
+        if (results[0]) {
+          self.provision(results[0], self.mpu9250);
+          self.server.info('Provisioned known device ' + self.name);
+        } else {
+          self.discover(self.mpu9250);
+          self.server.info('Discovered new device ' + self.name);
+        }
+      }
+      else {
+        self.server.error(err);
+      }
+    });
+
+    next();
+  }
+
+}
---
+++ mpu9250.js
@@ -0,0 +1,24 @@
+const LynxariDevice = require(process.lynxari.device);
+const device = require('@agilatech/mpu9250');
+
+module.exports = class Mpu extends LynxariDevice {
+    
+    constructor(config) {
+        
+        // The bus/file must be defined. If not supplied in config, then default to i2c-1
+        const bus  = config['bus'] || "/dev/i2c-2";
+
+        // The addr should be defined as well.
+        const addr = config['addr'] || 0x68;
+
+        // The rate is how often we want the hardware to update its values internally
+        const rate = config['rate'] || 500;
+        
+        const hardware = new device(bus, addr, rate);
+        
+        super(hardware, config);
+        
+    }
+    
+}
+
